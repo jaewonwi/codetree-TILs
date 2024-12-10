96f6de7bb050f54aa79dd9f91a0ff49bcc2db6ee
위재원 <80496853+jaewonwi@users.noreply.github.com>
2024-12-10 19:21:24 +0900

    Title: 합과 차의 나눗셈, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241210/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double result = (double)(a+b)/(a-b);
+        System.out.println(Math.round(result*100)/100.0);    
+    }
+}
