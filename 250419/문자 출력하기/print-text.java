2ab8bc6708faae814ac5c9f6cbd684a3a3cb1e7c
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-19 01:16:21 +0900

    Title: 문자 출력하기, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250419/문자 출력하기/print-text.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char str = sc.next().charAt(0);
+        for (int i = 0; i < 8; i++){
+            System.out.print(str);
+        }
+    }
+}
