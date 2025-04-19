2f05dfa529eb7ead3b3bbccca9388d183afc7d40
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-19 19:43:25 +0900

    Title: 세 정수형 변수 선언, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250419/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // printf에서 출력 형식
+        // %d-정수, %f-실수, %c-문자, %s-문자열
+        int a = 7, b = 23, c = 30;
+        System.out.printf("%d + %d = %d", a,b,c);
+    }
+}
