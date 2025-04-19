24640b24594d77d532a3573d172cc31c33c59a36
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-19 19:39:17 +0900

    Title: 정수 선언하고 뺄셈, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250419/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 한 줄에 선언하기 -> int a = 3, b = 5;
+        // java는 camelCase 방식 사용
+        int a = 97, b = 13;
+        System.out.printf("%d - %d = %d", a, b, a-b);
+    }
+}
