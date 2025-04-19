fbcf6af979f4517490d0b7865351504ba844c374
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-19 23:01:31 +0900

    Title: 최대공약수 구하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250419/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 250419/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -5,15 +5,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println(getMaxComDiv(n,m));
+        System.out.println(getGCD(n,m));
     }
 
-    static int getMaxComDiv(int n, int m){
-        int ans = 0;
-        int min = n >= m ? m : n;
-        for (int i = 1; i < min; i++){
-            if (n % i == 0 && m % i == 0) ans = i;
+    // 최대공약수(GCD), 최소공배수(LCM) 구하기 -> 유클리드 호제법!
+    // a > b, a % b = r 라고 할 때,
+    // "a와 b의 GCD == b와 r의 GCD"를 이용하여 계속해서 나머지를 구함. -> 나머지가 0일때 나누는 수가 GCD !
+    static int getGCD(int n, int m){
+        if (n < m) {
+            int temp = n;
+            n = m;
+            m = temp;
         }
-        return ans;
+        
+        while (n % m != 0){
+            int r = n % m;
+            n = m;
+            m = r;
+        }
+        
+        return m;
     }
 }
