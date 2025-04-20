19b8348b6e3e81da0f5b4112d0550bcf00be7013
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-21 01:32:50 +0900

    Title: 함수를 이용한 369 게임, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250421/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int cnt = 0;
+        for (int i = A; i <= B; i++){
+            if (is369(i))
+                cnt++;
+        }
+        System.out.println(cnt);
+    }
+
+    static boolean is369(int n){
+        if (n%3==0 || isExist369(n))
+            return true;
+        return false;
+    }
+
+    static boolean isExist369(int n){
+        while (n > 10){
+            int div = n / 10;
+            if (div == 3 || div == 6 || div == 9) return true;
+            n %= 10;
+        }
+        
+        if (n < 10){
+            if (n == 3 || n == 6 || n == 9) return true;
+        }
+
+        return false;
+    }
+}
