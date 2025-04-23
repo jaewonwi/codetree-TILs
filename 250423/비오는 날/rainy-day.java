be503677dc72d42db442e504de40a34973557036
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-23 15:35:24 +0900

    Title: 비오는 날, Time: 351ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250423/비오는 날/rainy-day.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Day[] days = new Day[n];
+        for (int i = 0; i < n; i++) {
+            String date = sc.next();
+            String day = sc.next();
+            String weather = sc.next();
+            days[i] = new Day(date, day, weather);
+        }
+
+        LocalDate today = LocalDate.now();
+        Day ans = days[0];
+        long minDiff = Long.MAX_VALUE;
+        for (int i = 0; i < n; i++){
+            Day cur = days[i];
+            if (!cur.weather.equals("Rain")) continue;
+
+            long diff = ChronoUnit.DAYS.between(today, cur.date);
+            if (diff > 0 &&  diff < minDiff){
+                // System.out.println(cur.date.toString()+" : "+diff+"차이 ");
+                minDiff = diff;
+                ans = cur;
+            }
+        }
+
+        System.out.println(ans.date+" "+ans.day+" "+ans.weather);
+    }
+
+    public static class Day{
+        LocalDate date;
+        String day, weather;
+
+        public Day(String ymd, String day, String weather){
+            this.date = LocalDate.parse(ymd);       //
+            this.day = day;
+            this.weather = weather;
+        }
+    }
+}
