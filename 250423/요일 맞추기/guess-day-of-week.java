6e76309e12e75b498398ccaf044b5c3b24d52f0c
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-23 21:53:11 +0900

    Title: 요일 맞추기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250423/요일 맞추기/guess-day-of-week.java
+++ 250423/요일 맞추기/guess-day-of-week.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+    static int[] days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    static String[] week = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt();
-        int m1 = sc.nextInt();
-        int m1 = sc.nextInt();
-        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int day1 = getDays(m1, d1);
+        int day2 = getDays(m2, d2);
+        
+        int rem1 = day1 % 7;
+        int rem2 = day2 % 7;
+        int diff = (rem2 - rem1 + 7) % 7;
+        System.out.println(week[diff]);
+    }
+
+    static int getDays(int m, int d){
+        int sum = d;
+        for (int i = 1; i < m; i++){
+            sum += days[i];
+        }
+        return sum;
     }
 }
