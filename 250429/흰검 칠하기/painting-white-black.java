795c90d754414ca40d91104533a5ba93302152dc
위재원 <80496853+jaewonwi@users.noreply.github.com>
2025-04-29 19:51:45 +0900

    Title: 흰검 칠하기, Time: 154ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250429/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int offset = 10000;
+        int cur = offset;
+        int[] color = new int[offset * 2 + 1];    // 마지막으로 칠한 색 (white 1, black 2, grey -1)
+        int[] cnt = new int[offset * 2 + 1];    // 덧칠한 횟수
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            int dir = 0;
+            int c = 0;
+            if (d == 'L'){
+                dir = -1;
+                c = 1;
+            } else {
+                dir = 1;
+                c = 2;
+            }
+
+            for (int j = 0; j < x; j++){
+                cnt[cur]++;
+                color[cur] = cnt[cur] < 4 ? c : -1;
+                cur += dir;
+            }
+            cur -= dir;
+        }
+        
+        int white = 0;
+        int black = 0;
+        int grey = 0;
+        
+        for (int i = 0; i < color.length; i++){
+            if (color[i] == 1)
+                white++;
+            else if (color[i] == 2)
+                black++;
+            else if (color[i] == -1)
+                grey++;
+        }
+
+        System.out.println(white+" "+black+" "+grey);
+    }
+}
