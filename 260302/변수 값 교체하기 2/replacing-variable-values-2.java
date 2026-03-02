6b95a57ea3462664639f4112c7aa8906bcf8beee
위재원 <80496853+jaewonwi@users.noreply.github.com>
2026-03-02 16:11:29 +0900

    Title: 변수 값 교체하기 2, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

--- 260302/변수 값 교체하기 2/replacing-variable-values-2.java
+++ 260302/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
-        b = a;
+        a = b;
         System.out.println(a);
         System.out.println(b);
     }
