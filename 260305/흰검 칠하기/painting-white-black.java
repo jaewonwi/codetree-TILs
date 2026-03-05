25efca65a4a1c08e5cdceb48a6bc632f72a0b37b
위재원 <80496853+jaewonwi@users.noreply.github.com>
2026-03-05 22:51:58 +0900

    Title: 흰검 칠하기, Time: 495ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 260305/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int offset = 100_000;
+        int cur = offset;
+        int[] color = new int[offset * 2 + 1];    // 마지막으로 칠한 색 (white 1, black 2, grey -1)
+        int[] cntB = new int[offset * 2 + 1];    // 검정 덧칠한 횟수
+        int[] cntW = new int[offset * 2 + 1];    // 흰색 덧칠한 횟수
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            int dir = 0;
+            int c = 0;
+            if (d == 'L'){
+                dir = -1;
+                c = 1;
+
+                for (int j = 0; j < x; j++){
+                    cntW[cur]++;
+                    color[cur] = (cntW[cur] >= 2 && cntB[cur] >= 2) ? -1 : c;
+                    cur += dir;
+                }
+            } else {
+                dir = 1;
+                c = 2;
+
+                for (int j = 0; j < x; j++){
+                    cntB[cur]++;
+                    color[cur] = (cntW[cur] >= 2 && cntB[cur] >= 2) ? -1 : c;
+                    cur += dir;
+                }
+            }
+            cur -= dir;
+        }
+        
+        long white = Arrays.stream(color).filter(c -> c == 1).count();
+        long black = Arrays.stream(color).filter(c -> c == 2).count();
+        long grey = Arrays.stream(color).filter(c -> c == -1).count();
+
+        System.out.println(white+" "+black+" "+grey);
+    }
+}
