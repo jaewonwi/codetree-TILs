133d689799916955f258b5e82536cf9f5ebf9bdd
위재원 <80496853+jaewonwi@users.noreply.github.com>
2026-03-05 21:43:28 +0900

    Title: 여러가지 진수변환, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260305/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int cnt = 0;
+        int[] binary = new int[32];
+        while (n > 0){
+            binary[i] = n % b;
+            n /= b;
+            cnt++;
+        }
+
+        for (int i = cnt-1; i >= 0; i--){
+            System.out.print(binary[i]);
+        }
+    }
+}
