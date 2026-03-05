bfcad890460d06fc13cc4bdc2a009a86ca2a2dfb
위재원 <80496853+jaewonwi@users.noreply.github.com>
2026-03-05 21:36:20 +0900

    Title: 십진수로 변환하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260305/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binary = sc.next();
+        char[] bChars = binary.toCharArray();
+        int[] bArr = new int[binary.length];
+        for (int i = 0; i < bChars.length; i++){
+            bArr[i] = bChars - '0';
+        }
+        
+        int num = 0;
+        for (int i = 0; i < bArr.length; i++){
+            num = num * 2 + bArr[i];
+        }
+        System.out.println(num);
+    }
+}
