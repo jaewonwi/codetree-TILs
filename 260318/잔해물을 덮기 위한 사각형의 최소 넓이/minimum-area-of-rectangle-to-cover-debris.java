5a5cc93531e55a4be87bfca450a847d34d8c0cd6
위재원 <80496853+jaewonwi@users.noreply.github.com>
2026-03-18 20:12:08 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 131ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 260318/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rect1_x1 = sc.nextInt()+1000;
+        int rect1_y1 = sc.nextInt()+1000;
+        int rect1_x2 = sc.nextInt()+1000;
+        int rect1_y2 = sc.nextInt()+1000;
+
+        int rect2_x1 = sc.nextInt()+1000;
+        int rect2_y1 = sc.nextInt()+1000;
+        int rect2_x2 = sc.nextInt()+1000;
+        int rect2_y2 = sc.nextInt()+1000;
+        
+        boolean[][] map = new boolean[2001][2001];
+        // 1. 겹치는 부분 제외
+        // 2. 1번사각형 범위 보면서 -> 제일 왼쪽,오른쪽,위,아래 좌표 찾기
+        for (int i = rect1_x1; i < rect1_x2; i++){
+            for (int j = rect1_y1; j < rect1_y2; j++){
+                map[i][j] = true;
+            }
+        }
+        
+        for (int i = rect2_x1; i < rect2_x2; i++){
+            for (int j = rect2_y1; j < rect2_y2; j++){
+                map[i][j] = false;
+            }
+        }
+
+        int left = 3000, bottom = 3000;
+        int top = 0, right = 0;
+        for (int i = rect1_x1; i < rect1_x2; i++){
+            for (int j = rect1_y1; j < rect1_y2; j++){
+                if (!map[i][j]) continue;
+
+                if (left > j){
+                    left = j;
+                }
+
+                if (bottom > i){
+                    bottom = i;
+                }
+
+                if (top < i){
+                    top = i;
+                }
+
+                if (right < j){
+                    right = j;
+                }
+            }
+        }
+        
+        System.out.print((right - left) * (top - bottom));
+    }
+}
