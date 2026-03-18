41198718ae1470f55932fd6c2c705acd08051b74
위재원 <80496853+jaewonwi@users.noreply.github.com>
2026-03-18 22:14:07 +0900

    Title: 계속 중첩되는 사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260318/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] map = new int[201][201];
+        boolean flag = true;    // t: red, f: blue
+        int offset = 100;
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt() + offset;
+            int y1 = sc.nextInt() + offset;
+            int x2 = sc.nextInt() + offset;
+            int y2 = sc.nextInt() + offset;
+
+            for (int j = x1; j < x2; j++){
+                for (int k = x1; k < x2; k++){
+                    map[j][k] = flag;
+                }
+            }
+            flag = !flag;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 201; i++){
+            for (int j = 0; j < 201; j++){
+                if (!map[i][j]) ans++;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
